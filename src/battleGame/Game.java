89a3645fddd15ecd3d6e0820deaa3c3ada774355
thaovo <ttthao.vo@gmail.com>
2016-12-22 13:10:12 +0000

    Refactoring to OOD

--- src/battleGame/Game.java
+++ src/battleGame/Game.java
@@ -4,43 +4,35 @@ import java.util.Random;
 
 public class Game {
 
-	public static int health_of_player1 = 50;
-	public static int health_of_player2 = 25;
-
-	public static int getFirstPlayerHP() {
-		return health_of_player1;
+	private Player player1;
+	private Player player2;
+	
+	public Game(int hpPlayer1, int hpPlayer2) {
+		player1 = new Player(hpPlayer1);
+		player2 = new Player(hpPlayer2);
+		play();
 	}
-
-	public static int getSecondPlayerHP() {
-		return health_of_player2;
-	}
-
-	public static int attack(int number) {
+	
+	private void attack(int number, Player player) {
 		Random rand = new Random();
-		return rand.nextInt(number) + 0;
+		player.setHpPlayer(player.getHpPlayer() - rand.nextInt(number) + 0);
 	}
 
-	public static void attackPlayer1() {
-		health_of_player1 -= attack(10);
-	}
-
-	public static void attackPlayer2() {
-		health_of_player2 -= attack(5);
-	}
-
-	public static void main(String[] args) {
+	private void play() {
 		int i = 1;
 
-		while (health_of_player1 > 0 && health_of_player2 > 0) {
+		while (player1.getHpPlayer() > 0 && player2.getHpPlayer() > 0) {
 			System.out.println("Round: " + i);
-			attackPlayer1();
-			attackPlayer2();
-			System.out.println("The remaining HP of player 1 is: " + health_of_player1);
-			System.out.println("The remaining HP of player 2 is: " + health_of_player2);
+			attack(10, player1);
+			attack(5, player2);
+			System.out.println("The remaining HP of player 1 is: " + player1.getHpPlayer());
+			System.out.println("The remaining HP of player 2 is: " + player2.getHpPlayer());
 			i++;
 		}
 
-		if (health_of_player1 > health_of_player2) {
+		if ((player1.getHpPlayer() < 0 && player2.getHpPlayer() < 0) || (player1.getHpPlayer() == 0 && player2.getHpPlayer() == 0)) {
+			System.out.println("DRAW!");
+		} else if (player1.getHpPlayer() > player2.getHpPlayer()) {
 			System.out.println("And the winner is: Player 1");
 		} else {
 			System.out.println("And the winner is: Player 2");
---
+++ src/battleGame/Player.java
@@ -0,0 +1,18 @@
+package battleGame;
+
+public class Player {
+
+	private int hpPlayer;
+	
+	public Player(int hpPlayer) {
+		this.setHpPlayer(hpPlayer);
+	}
+
+	public int getHpPlayer() {
+		return hpPlayer;
+	}
+
+	public void setHpPlayer(int hpPlayer) {
+		this.hpPlayer = hpPlayer;
+	}
+}
